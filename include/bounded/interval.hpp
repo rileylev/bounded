76307d351cfe8b11f05722901157a06ed759b5ef
Riley Levy <rileylevy95@gmail.com>
2022-05-12 01:07:56 -0400

    Add tests, use early returns instead of breaks

--- include/bounded/interval.hpp
+++ include/bounded/interval.hpp
@@ -10,9 +10,21 @@
 #include <algorithm>
 #include <compare>
 
+#define ASSERT(...) assert(__VA_ARGS__)
+
 namespace bounded {
 
 namespace impl {
+  // for switching
+  enum class porder : char { lt = -1, eq = 0, gt = 1, un = 2 };
+
+  constexpr porder to_porder(std::partial_ordering x) noexcept {
+    if(std::is_lt(x)) return porder::lt;
+    else if(std::is_eq(x)) return porder::eq;
+    else if(std::is_gt(x)) return porder::gt;
+    else return porder::un;
+  }
+
   template<class Less = std::less<>>
   constexpr auto project_less(auto proj, Less less = {}) {
     return [ proj, less ](auto x, auto y) ARROW(less(proj(x), proj(y)));
@@ -29,35 +41,174 @@ enum class Clusive : bool {
 constexpr Clusive operator*(Clusive x, Clusive y)
     NOEX(static_cast<Clusive>(static_cast<bool>(x) and static_cast<bool>(y)))
 
+template<class Poset>
+struct end;
+namespace impl {
+  struct end_friends {
+    friend constexpr bool operator==(end_friends, end_friends) = default;
+    template<std::three_way_comparable P>
+    friend constexpr auto operator+(end<P> x)
+        ARROW(end{+x.point, x.clusive})
+    template<std::three_way_comparable P>
+    friend constexpr auto operator-(end<P> x)
+        ARROW(end{-x.point, x.clusive})
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto operator+(end<X> x, end<Y> y)
+        ARROW(end{x.point + y.point, x.clusive* y.clusive})
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto operator-(end<X> x, end<Y> y) ARROW(x + (-y))
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto operator*(end<X> x, end<Y> y)
+        ARROW(end{x.point * y.point, x.clusive* y.clusive})
+  };
+} // namespace impl
 /**
  * Represents either end of an interval
  */
-template<std::three_way_comparable Poset>
-struct end {
-  Poset   point;
-  Clusive clusive;
+template<class Poset>
+struct end : impl::end_friends {
+  Poset       point;
+  Clusive     clusive;
+  friend bool operator==(end, end) = default;
+
+  constexpr end() = default;
+  constexpr end(Poset point_, Clusive clusive_)
+      : point{point_}, clusive{clusive_} {}
 };
 template<class X>
 end(X, Clusive) -> end<X>;
 
-template<std::three_way_comparable P>
-constexpr auto operator+(end<P> x) ARROW(end{+x.point, x.clusive})
-template<std::three_way_comparable P>
-constexpr auto operator-(end<P> x) ARROW(end{-x.point, x.clusive})
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator+(end<X> x, end<Y> y)
-    ARROW(end{x.point + y.point, x.clusive* y.clusive})
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator-(end<X> x, end<Y> y) ARROW(x + (-y))
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator*(end<X> x, end<Y> y)
-    ARROW(end{x.point * y.point, x.clusive* y.clusive})
+template<class T>
+constexpr end<T> ex(T x) RET(end{x, Clusive::ex})
+template<class T>
+constexpr end<T> in(T x)RET(end{x, Clusive::in})
 
 using passing_traits::Read;
-using passing_traits::ReadOut;
+
+template<std::three_way_comparable Poset>
+struct interval;
+namespace impl {
+  struct interval_friends {
+    friend constexpr bool
+        operator==(interval_friends, interval_friends) = default;
+    // Allow or disallow operators on intervals over different types?
+    // Probably should defer to the underlying types in general
+    // If it makes sense to add X + Y, it probably makes sense to add
+    // interval<X>
+    // + interval<Y> I just wish the builtins were stricter. Allowed =/=
+    // makes sense.
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto
+        operator==(interval<X> const& x, interval<Y> const& y)
+            ARROW(x.btm_end() == y.btm_end() and x.top_end() == y.top_end())
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto
+        operator+(interval<X> const& x, interval<Y> const& y)
+            ARROW((x.empty() or y.empty())
+                      ? interval<std::invoke_result_t<std::plus<>, X, Y>>{}
+                      : interval{x.btm_end() + y.btm_end(),
+                                 x.top_end() + y.top_end()})
+
+    template<std::three_way_comparable X>
+    friend constexpr auto operator-(interval<X> const& x)
+        ARROW(interval{-x.top_end(), -x.btm_end()})
+    template<std::three_way_comparable X>
+    friend constexpr auto operator+(interval<X> const& x)
+        ARROW(interval{+x.btm_end(), +x.top_end()})
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto
+        operator-(interval<X> const& x, interval<Y> const& y)
+            ARROW(x + (-y))
+
+    template<std::three_way_comparable X, std::three_way_comparable Y>
+    friend constexpr auto
+        operator*(interval<X> const& x, interval<Y> const& y)
+            -> interval<std::invoke_result_t<std::multiplies<>, X, Y>> {
+      using prod_t = decltype(std::declval<X>() * std::declval<Y>());
+
+      if(x.empty() or y.empty()) return {};
+
+      std::array<end<prod_t>, 4> prods{
+          // clang-format off
+                                  /* y.btm_end   ,             y.top_end */
+        /*x.btm_end*/  x.btm_end() * y.btm_end() , x.btm_end * y.top_end(),
+        /*x.top_end*/  x.top_end() * y.btm_end() , x.top_end * y.top_end()
+          // clang-format on
+      };
+      auto const btm_end = *std::min_element(
+          prods.begin(),
+          prods.end(),
+          impl::project_less(
+              [] FN(std::pair{_.point, _.clusive == Clusive::ex})));
+      auto const top_end = *std::max_element(
+          prods.begin(),
+          prods.end(),
+          project_less([] FN(std::pair{_.point, _.clusive == Clusive::in})));
+      return interval{btm_end, top_end};
+    }
+    // tricky cases [-1,1) * [-1,1] = [-1,1]
+
+    /**
+     * Three-way-compare intervals by set inclusion: A < B iff A ⊂ B
+     *
+     *  - A <=> B <  0          iff  A ⊂ B
+     *  - A <=> B == 0          iff  A = B
+     *  - A <=> B >  0          iff  A ⊃ B
+     *  - A <=> B == unordered  iff  A ≠ B, A ⊄ B, A ⊅ B
+     *
+     *  Examples:
+     *  [0,1]  <  [0,2]
+     *  [0,1) <=> (0,1] = unordered
+     */
+    // TODO: what does weak equivalence mean for subset inclusion?
+    template<class X, class Y>
+    friend constexpr std::partial_ordering
+        operator<=>(interval<X> const& x_, interval<Y> const& y_) {
+      // next best thing to inferring to pass by value (the compiler didn't
+      // like that)
+      Read<interval<X>> x = x_;
+      Read<interval<Y>> y = y_;
+      if(x.empty())
+        return (y.empty()) ? std::partial_ordering::equivalent
+                           : std::partial_ordering::less;
+      else if(y.empty()) return std::partial_ordering::greater;
+      else {
+        // clusivity breaks ties
+        // [0,1) vs (0,1) --- the exclusive interval starts a hair higher
+        auto const btms =
+            std::pair{x.btm(), x.btm_clusive() == Clusive::ex}
+            <=> std::pair{y.btm(), y.btm_clusive() == Clusive::ex};
+        // [0,1) vs [0,1] --- the inclusive interval ends a hair higher
+        auto const tops =
+            std::pair{x.top(), x.top_clusive() == Clusive::in}
+            <=> std::pair{y.top(), y.top_clusive() == Clusive::in};
+        using namespace impl;
+        switch(to_porder(btms)) {
+          case porder::lt:
+            switch(to_porder(tops)) {
+              case porder::gt:
+              case porder::eq: return std::partial_ordering::greater;
+              default: return std::partial_ordering::unordered;
+            }
+          case porder::eq: return tops;
+          case porder::gt:
+            switch(to_porder(tops)) {
+              case porder::lt:
+              case porder::eq: return std::partial_ordering::less;
+              default: return std::partial_ordering::unordered; ;
+            }
+         default: return std::partial_ordering::unordered;
+        }
+      }
+    }
+  };
+} // namespace impl
+
 /**
  * Represents an interval in any `std::three_way_comparable` Poset.
  *
@@ -65,7 +216,7 @@ using passing_traits::ReadOut;
  * parameter. A name ending with a trailing underscore is morally private.
  */
 template<std::three_way_comparable Poset>
-struct interval {
+struct interval : impl::interval_friends {
   /* To use this as a non-type template parameter, we can't use private.
 
    https://en.cppreference.com/w/cpp/language/template_parameters:
@@ -106,24 +257,22 @@ struct interval {
   constexpr interval(end<Poset> btm, end<Poset> top) noexcept(
       std::is_nothrow_move_constructible_v<Poset>and noexcept(
           interval{},
-          btm() <=> top()))
+          this->btm() <=> this->top()))
       : btm_{std::move(btm.point)},
         top_{std::move(top.point)},
-        btm_clusive_{btm.clusve},
+        btm_clusive_{btm.clusive},
         top_clusive_{top.clusive} {
     // normalize the degenerate cases
-    switch(btm() <=> top()) {
-      case std::partial_ordering::greater:
-        // unordered ->  interval must be empty by transitivity
-      case std::partial_ordering::unordered: *this = interval{}; break;
-      case std::partial_ordering::equivalent:
+    using namespace impl;
+    switch(to_porder(this->btm() <=> this->top())) {
+      case porder::gt:
+      case porder::un: *this = interval{}; break;
+      case porder::eq:
         if((btm_clusive() == Clusive::ex) or (top_clusive() == Clusive::ex))
           *this = interval{};
-      case std::partial_ordering::less: return;
+      case porder::lt: return;
     }
   }
-
-  constexpr friend bool operator==(interval, interval) = default;
 
   /**
    * Does this interval contain x? x∈*this?
@@ -143,106 +292,5 @@ struct interval {
   }
 };
 
-// Allow or disallow operators on intervals over different types?
-// Probably should defer to the underlying types in general
-// If it makes sense to add X + Y, it probably makes sense to add interval<X>
-// + interval<Y> I just wish the builtins were stricter. Allowed =/= makes sense.
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator==(interval<X> x, interval<Y> y)
-    ARROW(x.btm_end() == y.btm_end() and x.top_end() == y.top_end())
-
-/**
- * Three-way-compare intervals by set inclusion: A < B iff A ⊂ B
- *
- *  - A <=> B <  0          iff  A ⊂ B
- *  - A <=> B == 0          iff  A = B
- *  - A <=> B >  0          iff  A ⊃ B
- *  - A <=> B == unordered  iff  A ≠ B, A ⊄ B, A ⊅ B
- *
- *  Examples:
- *  [0,1]  <  [0,2]
- *  [0,1) <=> (0,1] = unordered
- */
- // TODO: what does weak equivalence mean for subset inclusion?
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr std::partial_ordering
-    operator<=>(Read<interval<X>> x, Read<interval<Y>> y) {
-  if(x.empty())
-    return (y.empty()) ? std::partial_ordering::equivalent
-                       : std::partial_ordering::less;
-  else if(y.empty()) return std::partial_ordering::greater;
-  else {
-    // clusivity breaks ties
-    // [0,1) vs (0,1) --- the exclusive interval starts a hair higher
-    auto const btms = std::pair{x.btm(), x.clusive() == Clusive::ex}
-                  <=> std::pair{y.btm(), y.clusive() == Clusive::ex};
-    // [0,1) vs [0,1] --- the inclusive interval ends a hair higher
-    auto const tops = std::pair{x.top(), x.clusive() == Clusive::in}
-                  <=> std::pair{y.top(), y.clusive() == Clusive::in};
-
-    switch(btms) {
-      case std::partial_ordering::less:
-        switch(tops) {
-          case std::partial_ordering::greater:
-          case std::partial_ordering::equivalent:
-            return std::partial_ordering::greater;
-        }
-        break;
-      case std::partial_ordering::equivalent: return tops;
-      case std::partial_ordering::greater:
-        switch(tops) {
-          case std::partial_ordering::less:
-          case std::partial_ordering::equivalent:
-            return std::partial_ordering::less;
-        }
-        break;
-    }
-    return std::partial_ordering::unordered;
-  }
-}
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator+(Read<interval<X>> x, Read<interval<Y>> y) ARROW(
-    (x.empty() or y.empty())
-        ? interval<std::invoke_result_t<std::plus<>, X, Y>>{}
-        : interval{x.btm_end() + y.btm_end(), x.top_end() + y.top_end()})
-
-template<std::three_way_comparable X>
-constexpr auto operator-(Read<interval<X>> x)
-    ARROW(interval{-x.top_end(), -x.btm_end()})
-
-template<std::three_way_comparable X>
-constexpr auto operator+(Read<interval<X>> x)
-    ARROW(interval{+x.btm_end(), +x.top_end()})
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator-(Read<interval<X>> x, Read<interval<Y>> y)
-    ARROW(x + (-y))
-
-template<std::three_way_comparable X, std::three_way_comparable Y>
-constexpr auto operator*(Read<interval<X>> x, Read<interval<Y>> y)
-    -> interval<std::invoke_result_t<std::multiplies<>, X, Y>> {
-  using prod_t = decltype(std::declval<X>() * std::declval<Y>());
-
-  if(x.empty() or y.empty()) return {};
-
-  std::array<end<prod_t>, 4> prods{
-      // clang-format off
-                                  /* y.btm_end   ,             y.top_end */
-        /*x.btm_end*/  x.btm_end() * y.btm_end() , x.btm_end * y.top_end(),
-        /*x.top_end*/  x.top_end() * y.btm_end() , x.top_end * y.top_end()
-      // clang-format on
-  };
-  auto const btm_end =
-      *std::min_element(prods.begin(), prods.end(), impl::project_less([
-                        ] FN(std::pair{_.point, _.clusive == Clusive::ex})));
-  auto const top_end =
-      *std::max_element(prods.begin(), prods.end(), project_less([
-                        ] FN(std::pair{_.point, _.clusive == Clusive::in})));
-  return interval{btm_end, top_end};
-}
-// tricky cases [-1,1) * [-1,1] = [-1,1]
-
-static_assert(!interval<int>{}.has(0));
 } // namespace bounded
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,63 @@
 #define CATCH_CONFIG_MAIN
 
-#include <bounded/interval.hpp>
 #include <catch2/catch.hpp>
+#include <bounded/interval.hpp>
 
+using namespace bounded;
+using namespace std::literals;
+
+TEST_CASE("All empty intervals (of the same type) are ==") {
+  STATIC_REQUIRE(interval{ex(0), ex(0)} == interval<int>{});
+  STATIC_REQUIRE(interval{ex(0), ex(0)} == interval{ex(1), ex(-1)});
+  STATIC_REQUIRE(interval{ex(2), in(2)} == interval{in(1), in(-1)});
+
+  STATIC_REQUIRE(std::three_way_comparable<interval<int>>);
+
+  STATIC_REQUIRE(interval{ex(interval<int>{}), ex(interval<int>{})}
+                 == interval<interval<int>>{});
+}
+
+TEST_CASE("If both endpoints are the same and at least one is exclusive, "
+          "the interval is empty") {
+  STATIC_REQUIRE(interval{ex(0), ex(0)}.empty());
+  STATIC_REQUIRE(interval{in(0), ex(0)}.empty());
+  STATIC_REQUIRE(interval{ex(0), in(0)}.empty());
+
+  STATIC_REQUIRE(interval{ex(interval<int>{}), ex(interval<int>{})}.empty());
+  STATIC_REQUIRE(interval{in(interval<int>{}), ex(interval<int>{})}.empty());
+  STATIC_REQUIRE(interval{ex(interval<int>{}), in(interval<int>{})}.empty());
+
+  STATIC_REQUIRE(interval{ex( "a"sv ), ex("a"sv)}.empty());
+  STATIC_REQUIRE(interval{in( "a"sv ), ex("a"sv)}.empty());
+  STATIC_REQUIRE(interval{ex( "a"sv ), in("a"sv)}.empty());
+}
+
+TEST_CASE("An empty interval contains no element") {
+  STATIC_REQUIRE(!interval<int>{}.has(0));
+}
+
+TEST_CASE("Inclusive endpoints are contained") {
+  STATIC_REQUIRE(interval{in(0), in(0)}.has(0));
+
+  STATIC_REQUIRE(interval{in(0), in(1)}.has(0));
+  STATIC_REQUIRE(interval{in(0), in(1)}.has(1));
+
+  STATIC_REQUIRE(interval{in("a"sv), in("b"sv)}.has("a"sv));
+  STATIC_REQUIRE(interval{in("a"sv), in("b"sv)}.has("b"sv));
+}
+
+TEST_CASE("Exclusive endpoints are not contained") {
+  STATIC_REQUIRE(!interval{ex(0), ex(0)}.has(0));
+
+  STATIC_REQUIRE(!interval{ex(0), ex(1)}.has(0));
+  STATIC_REQUIRE(!interval{ex(0), ex(1)}.has(1));
+
+  STATIC_REQUIRE(!interval{ex("a"sv), ex("b"sv)}.has("a"sv));
+  STATIC_REQUIRE(!interval{ex("a"sv), ex("b"sv)}.has("b"sv));
+}
+
+TEST_CASE("The innards are inside but the outsides are not"){
+  STATIC_REQUIRE(interval{ex(-1), ex(1)}.has(0));
+  STATIC_REQUIRE(!interval{ex(-1), ex(1)}.has(2));
+  STATIC_REQUIRE(!interval{ex(-1), ex(1)}.has(-2));
+}
